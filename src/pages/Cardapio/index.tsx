e432e54d3922c20bf95e10de3f859d913b70bfa0
Bruno Tenório <tenoriobn@gmail.com>
2023-10-16 10:19:26 -0300

    Estilo tema

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -5,6 +5,7 @@ import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
 import React from 'react';
+import stylesTema from 'styles/Tema.module.scss';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
@@ -12,7 +13,7 @@ export default function Cardapio() {
   const [ordenador, setOrdenador] = useState('');
   return (
     <section className={styles.cardapio}>
-      <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+      <h3 className={stylesTema.titulo}>Cardápio</h3>
       <Buscador busca={busca} setBusca={setBusca} />
       <div className={styles.cardapio__filtros}>
         <Filtros filtro={filtro} setFiltro={setFiltro} />
